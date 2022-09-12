b74784c4fb15222d52cde38e57721dcce1dd7e60
John Turner <7strbass@gmail.com>
2022-09-12 17:44:21 -0400

    --streamline vert norm calc

--- src/cs7492Project3/myMCTri.java
+++ src/cs7492Project3/myMCTri.java
@@ -1,27 +1,26 @@
 package cs7492Project3;
 
 public class myMCTri {	
-	public myPointf pt[] = new myPointf[3];
+	private myPointf pt[] = new myPointf[3];
 	public myMCVert verts[] = new myMCVert[3];
-	public myVectorf n;
 	public myMCTri(myPointf[] pts) {
 		for (int i = 0; i < 3; ++i) {
 			pt[i] = new myPointf(pts[i]);
 		} 
-		//n = new myVectorf(pt[0],pt[1])._cross(new myVectorf(pt[0],pt[2]));
 	}
+	
 	public myMCTri( myMCVert[] _v) {
 		for (int i = 0; i < 3; ++i) {
 			verts[i]=_v[i];
 			pt[i] = new myPointf(verts[i].loc);
 		} 
-		n = new myVectorf(pt[0],pt[1])._cross(new myVectorf(pt[0],pt[2]));
+		myVectorf  n = new myVectorf(pt[0],pt[1])._cross(new myVectorf(pt[0],pt[2]));
 		if(n.sqMagn < .000001f){
 			n = new myVectorf(pt[0],pt[2])._cross(new myVectorf(pt[2],pt[1]));
 		}
-		n._normalize();//	
+		//n._normalize();//	
 		for(int i =0; i<3; ++i){
-			verts[i].setVert(n);
+			verts[i].setNorm(n);
 		}
 	}
 
--- src/cs7492Project3/myMCVert.java
+++ src/cs7492Project3/myMCVert.java
@@ -2,37 +2,18 @@ package cs7492Project3;
 
 //class to store vertexes of triangle - will calculate normal based on all triangles that share this vertex
 public class myMCVert{
-	public int numTris = 0;
 	public myPointf loc = new myPointf();
 	public myVectorf n = new myVectorf();			//raw unit normal == actual normal * # tris sharing this vertex
-	public myMCVert(){
-	//	clearVert();
-	}
-	public void clearVert(){
-		numTris = 0;
-		loc.set(0,0,0);
-		n.set(0,0,0);
-	}
-	
+	public myMCVert(){	}
+		
 	public void setInitVert(myPointf _loc){
 		loc.set(_loc);		
 	}
 	
 	//set the location of this vert, and the initial normal and # tris
-	public void setVert(myVectorf _norm){
-		numTris++;				//increment # of triangles sharing this vertex
-		if(numTris == 1){
-			n.set(_norm);
-		} else {			
-			n._add(_norm);
-		}
+	public void setNorm(myVectorf _norm){
+		n._add(_norm);
 	}
-
-	//draw per vert normal
-//	public void drawMe(cs7492Proj3 pa){
-//		pa.gl_normal(n);                                         // changes normal for smooth shading		
-//		pa.gl_vertex(loc);
-//	}
 		
 }//
 
